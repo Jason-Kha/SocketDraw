c344c7a6a32f4716d6d6e1e92d658696af160378
Jason Kha <jasonkha63@yahoo.com>
2021-01-26 13:34:05 -0600

    Updated index

--- index.js
+++ index.js
@@ -8,13 +8,12 @@ app.get('/ProjectGamers', (req, res) => {
 
 io.on('connection', (socket) => {
     console.log('a user connected');
+
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+    });
 });
 
 http.listen(3000, () => {
     console.log('listening on *:3000');
-
-    console.log('hello');
-
-    console.log('welcome to project gamers');
 });
-
