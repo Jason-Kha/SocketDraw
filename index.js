c5e48b5d7bf5a8acbad70289062ef809f3eca91c
John B <noteon127@gmail.com>
2021-01-26 01:24:40 -0600

    Added various files including a T-Pain image

---
+++ index.js
@@ -0,0 +1,12 @@
+const { send } = require('process');
+
+const app = require('express')();
+const http = require('http').createServer(app);
+
+app.get('/ProjectGamers', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+});
+
+http.listen(3000, () => {
+    console.log('listening on *:3000');
+});
