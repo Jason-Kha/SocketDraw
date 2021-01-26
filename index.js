daec6c73470e9b8fbabc78b2185b01517866616d
Jason Kha <jasonkha63@yahoo.com>
2021-01-26 14:51:06 -0600

    Added client chat

--- index.js
+++ index.js
@@ -11,11 +11,18 @@ app.use(express.static(__dirname + '/public'));
 // socket.io setup
 const io = socketio(expressServer);
 
-// on user connection
+// on socket connection
 io.on('connection', (socket) => {
     console.log('a user connected');
 
+    // on user disconnect
     socket.on('disconnect', () => {
         console.log('user disconnected');
+    });
+
+    // on chat message
+    socket.on('chat message', (msg) => {
+        console.log('message: ' + msg);
+        io.emit('chat message', msg);
     });
 });
--- public/scripts.js
+++ public/scripts.js
@@ -1,5 +1,31 @@
+// change this to correct ip later
 const socket = io('http://localhost:3000');
 
+// on connection to server
 socket.on('connect', () => {
-    console.log(`socket 1: ${socket.id}`);
+    console.log(`socket: ${socket.id}`);
+});
+
+// on chat message
+socket.on('chat message', (msg) => {
+    // create listed item with message
+    var item = document.createElement('li');
+    item.textContent = msg;
+    messages.appendChild(item);
+    window.scrollTo(0, document.body.scrollHeight);
+});
+
+// set up chat by getting id's
+var form = document.getElementById('form');
+var input = document.getElementById('input');
+
+form.addEventListener('submit', function (e) {
+    // don't refresh screen
+    e.preventDefault();
+
+    // emit chat message to server
+    if (input.value) {
+        socket.emit('chat message', input.value);
+        input.value = '';
+    }
 });
