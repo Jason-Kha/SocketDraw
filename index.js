c1aa21cefc9a3d7dad1f9f5a462bb3fccfe91d1f
Jason Kha <jasonkha63@yahoo.com>
2021-01-26 20:23:57 -0600

    Made chatbox look more nice as well as added more socket events

--- index.js
+++ index.js
@@ -2,8 +2,6 @@ const express = require('express');
 const app = express();
 const socketio = require('socket.io');
 
-const config = require('config');
-
 // run express server on port 3000
 const expressServer = app.listen(3000);
 
@@ -22,9 +20,13 @@ io.on('connection', (socket) => {
         console.log('user disconnected');
     });
 
+    socket.on('newUser', (username) => {
+        io.emit('server message', username + ' has joined the chat');
+    });
+
     // on chat message
-    socket.on('chat message', (msg) => {
-        console.log('message: ' + msg);
+    socket.on('chat message', ({ msg }) => {
+        console.log(`${msg.name} says: ${msg.msg}`);
         io.emit('chat message', msg);
     });
 });
--- public/scripts.js
+++ public/scripts.js
@@ -1,6 +1,10 @@
 // change this to correct ip later
 const socket = io('localhost:3000');
 
+// retrieve username
+const username = prompt('Please enter your name');
+socket.emit('newUser', username);
+
 // on connection to server
 socket.on('connect', () => {
     console.log(`socket: ${socket.id}`);
@@ -8,6 +12,16 @@ socket.on('connect', () => {
 
 // on chat message
 socket.on('chat message', (msg) => {
+    // create listed item with message
+    var item = document.createElement('tr');
+    item.innerHTML = `<td>${msg.name}: ${msg.msg}</td>`;
+
+    messages.appendChild(item);
+
+    window.scrollTo(0, document.body.scrollHeight);
+});
+
+socket.on('server message', (msg) => {
     // create listed item with message
     var item = document.createElement('tr');
     item.innerHTML = `<td>${msg}</td>`;
@@ -27,7 +41,12 @@ form.addEventListener('submit', function (e) {
 
     // emit chat message to server
     if (input.value) {
-        socket.emit('chat message', input.value);
+        socket.emit('chat message', {
+            msg: {
+                name: username,
+                msg: input.value
+            }
+        });
         input.value = '';
     }
 });
